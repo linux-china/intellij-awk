0f60ac010c5a2e519ebdfa5aa31e0d60d87ab796
Libing Chen <libing.chen@gmail.com>
2024-02-27 00:10:18 +0800

    refactor: add zawk stdlib

--- src/main/java/intellij_awk/AwkFunctions.java
+++ src/main/java/intellij_awk/AwkFunctions.java
@@ -56,53 +56,110 @@ public final class AwkFunctions {
                     entry("dcgettext", "(string [, domain [, category] ])"),
                     entry("dcngettext", "(string1, string2, number [, domain [, category] ])"));
 
-    static final Map<String, String> frawkFunctions =
-            Map.ofEntries(
-                    entry("hex", "(number)"),
-                    entry("int", "(source)"),
-                    entry("join_fields", "(i, j [, sep])"),
-                    entry("escape_csv", "(source)"),
-                    entry("escape_tsv", "(source)"),
-                    entry("join_csv", "(i, j)"),
-                    entry("join_tsv", "(i, j)")
-            );
     static final Map<String, String> zawkFunctions =
             Map.ofEntries(
-                    entry("fend", "(number)"),
+                    // string functions
+                    entry("match", "(str, re)"),
+                    entry("hex", "(str)"),
+                    entry("join_fields", "(i, j [, sep])"),
+                    entry("join_csv", "(i, j)"),
+                    entry("join_tsv", "(i, j)"),
+                    entry("strtonum", "(str)"),
+                    entry("trim", "(str[,holder])"),
+                    entry("truncate", "(str,len[,holder])"),
+                    entry("capitalize", "(str)"),
+                    entry("camel_case", "(str)"),
+                    entry("kebab_case", "(str)"),
+                    entry("snake_case", "(str)"),
+                    entry("title_case", "(str)"),
+                    entry("isint", "(str)"),
+                    entry("isnum", "(str)"),
+                    entry("mask", "(str, mask)"),
+                    entry("pad", "(str, len, placeholder)"),
+                    entry("pad_start", "(str, len, placeholder)"),
+                    entry("pad_end", "(str, len, placeholder)"),
+                    entry("strcmp", "(str, str)"),
+                    entry("words", "(str)"),
+                    entry("repeat", "(str, n)"),
+                    // text escape functions
+                    entry("escape", "(format,str)"),
+                    entry("escape_csv", "(str)"),
+                    entry("escape_tsv", "(str)"),
+                    // text parser functions
+                    entry("url", "(str)"),
+                    entry("data_url", "(str)"),
+                    entry("shlex", "(str)"),
+                    entry("path", "(str)"),
+                    entry("semver", "(str)"),
+                    // id generator functions
+                    entry("uuid", "()"),
+                    entry("ulid", "()"),
+                    entry("snowflake", "()"),
+                    // array functions
+                    entry("seq", "(i [,step],j)"),
+                    entry("uniq", "(arr)"),
+                    entry("asort", "(arr)"),
+                    entry("_max", "(arr)"),
+                    entry("_min", "(arr)"),
+                    entry("_sum", "(arr)"),
+                    entry("_mean", "(arr)"),
+                    entry("_join", "(arr[, sep])"),
+                    // math functions
                     entry("abs", "(number)"),
                     entry("floor", "(number)"),
                     entry("ceil", "(number)"),
-                    entry("round", "(inumber)"),
+                    entry("round", "(number)"),
                     entry("min", "(i, j[, k])"),
                     entry("max", "(i, j[, k])"),
-                    entry("trim", "(str)"),
-                    entry("truncate", "(str[,holder])"),
-                    entry("escape", "(format,str)"),
-                    entry("capitalize", "(str)"),
-                    entry("seq", "(i [,step],j)"),
-                    entry("_join", "(arr[, sep])"),
+                    entry("fend", "(expression)"),
+                    entry("mkbool", "(str)"),
+                    entry("int", "(str)"),
+                    entry("float", "(str)"),
+                    //json functions
+                    entry("to_json", "(arr)"),
+                    entry("from_json", "(str)"),
+                    //json functions
+                    entry("to_csv", "(arr)"),
+                    entry("from_csv", "(str)"),
+                    // encoding
+                    entry("encode", "(format, str)"),
+                    entry("decode", "(format, str)"),
+                    // crypto
+                    entry("digest", "(algorithm, str)"),
+                    entry("hmac", "(algorithm, key, str)"),
+                    entry("jwt", "(algorithm, key, arr)"),
+                    // KV functions
+                    entry("kv_get", "(namespace, key)"),
+                    entry("kv_put", "(namespace, key, value)"),
+                    entry("kv_delete", "(namespace, key)"),
+                    entry("kv_clear", "(namespace)"),
+                    // network functions
                     entry("local_ip", "()"),
                     entry("http_get", "(url[,headers])"),
                     entry("http_post", "(url, [headers, body])"),
                     entry("s3_get", "(bucket, object_name)"),
                     entry("s3_put", "(bucket, object_name, body)"),
-                    entry("url", "(str)"),
                     entry("publish", "(nats_url, message)"),
+                    // database functions
+                    entry("sqlite_query", "(db_path, sql)"),
+                    entry("sqlite_execute", "(db_path, sql)"),
+                    entry("mysql_query", "(url, sql)"),
+                    entry("mysql_execute", "(url, sql)"),
+                    // date time functions
+                    entry("datetime", "(datespec)"),
+                    // os functions
                     entry("whoami", "()"),
-                    entry("encode", "(format, str)"),
-                    entry("decode", "(format, str)"),
-                    entry("digest", "(algorithm, str)"),
-                    entry("hmac", "(algorithm, key, str)"),
-                    entry("to_json", "(arr)"),
-                    entry("from_json", "(str[,holder])"),
-                    entry("kv_get", "(namespace, key)"),
-                    entry("kv_put", "(namespace, key, value)"),
-                    entry("kv_delete", "(namespace, key)"),
-                    entry("kv_clear", "(namespace)"),
-                    entry("uuid", "()"),
-                    entry("ulid", "()"),
-                    entry("snowflake", "()"),
-                    entry("datetime", "(datespec)")
+                    entry("os", "()"),
+                    entry("arch", "()"),
+                    entry("os_family", "()"),
+                    entry("pwd", "()"),
+                    entry("user_home", "()"),
+                    // misc functions
+                    entry("var_dump", "(variable)"),
+                    entry("log_debug", "(msg)"),
+                    entry("log_info", "(msg)"),
+                    entry("log_warn", "(msg)"),
+                    entry("log_error", "(msg)")
             );
 
     private static final Map<String, ParametersHint> parameterHintsCache = new HashMap<>();
@@ -118,9 +175,6 @@ public final class AwkFunctions {
         String signature = builtInFunctions.get(funcName);
         if (signature == null) {
             signature = gawkFunctions.get(funcName);
-        }
-        if (signature == null) {
-            signature = frawkFunctions.get(funcName);
         }
         if (signature == null) {
             signature = zawkFunctions.get(funcName);
